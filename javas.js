82288321b41ad0c37cb629b1f8de4883530835a8
Sharvari Sunil Pradhan <113926050+19SP@users.noreply.github.com>
2023-05-31 13:02:57 +0400

    final code

--- javas.js
+++ javas.js
@@ -1,8 +1,8 @@
 function show(){
-    document.querySelector('.hamburger').classList.toggle('open')
+    document.querySelector('.hamburger').classList.toggle('open') /*toggle helps to check the visibility of elements*/
     document.querySelector('.navigation').classList.toggle('active')
 }
 
 function sct() {
-    window.scrollTo(0, 0);
+    window.scrollTo(0, 0); /*this helps to scroll the page to the top i.e., 0,0 coordinates*/
 }

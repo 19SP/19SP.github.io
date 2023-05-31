b41342dbf7b1f4d206ec366088b6171fa5a613b2
Sharvari Sunil Pradhan <113926050+19SP@users.noreply.github.com>
2023-06-01 00:01:59 +0400

    final code+comments

--- javas.js
+++ javas.js
@@ -1,8 +1,10 @@
 function show(){
     document.querySelector('.hamburger').classList.toggle('open') /*toggle helps to check the visibility of elements*/
     document.querySelector('.navigation').classList.toggle('active')
+    /*hamburger menu by CodeMode available at https://www.youtube.com/watch?v=vkNkeCSb13M*/
 }
 
 function sct() {
-    window.scrollTo(0, 0); /*this helps to scroll the page to the top i.e., 0,0 coordinates*/
+    window.scrollTo(0, 0); 
+    /*this helps to scroll the page to the top i.e., 0,0 coordinates*/
 }
